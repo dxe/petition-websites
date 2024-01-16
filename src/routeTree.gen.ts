74ed5851a6647b25a240aa303af2b9b905b800e0
jakehobbs <jakehobbs@gmail.com>
2024-01-16 14:16:36 -0800

    fmt

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -1,24 +1,24 @@
-import { FileRoute, lazyRouteComponent } from '@tanstack/react-router'
+import { FileRoute, lazyRouteComponent } from "@tanstack/react-router";
 
-import { Route as rootRoute } from './routes/__root'
+import { Route as rootRoute } from "./routes/__root";
 
-const IndexComponentImport = new FileRoute('/').createRoute()
+const IndexComponentImport = new FileRoute("/").createRoute();
 
 const IndexComponentRoute = IndexComponentImport.update({
-  path: '/',
+  path: "/",
   getParentRoute: () => rootRoute,
 } as any).update({
   component: lazyRouteComponent(
-    () => import('./routes/index.component'),
-    'component',
+    () => import("./routes/index.component"),
+    "component",
   ),
-})
-declare module '@tanstack/react-router' {
+});
+declare module "@tanstack/react-router" {
   interface FileRoutesByPath {
-    '/': {
-      preLoaderRoute: typeof IndexComponentImport
-      parentRoute: typeof rootRoute
-    }
+    "/": {
+      preLoaderRoute: typeof IndexComponentImport;
+      parentRoute: typeof rootRoute;
+    };
   }
 }
-export const routeTree = rootRoute.addChildren([IndexComponentRoute])
+export const routeTree = rootRoute.addChildren([IndexComponentRoute]);
