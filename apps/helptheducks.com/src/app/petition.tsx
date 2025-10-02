184fbc88fc21c367146ef99598a787e1c638e178
Alexander Taylor <ajxtaylor@gmail.com>
2025-10-02 01:53:49 +0000

    Use server components

---
+++ apps/helptheducks.com/src/app/petition.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+import { EmailPetition } from "@dxe/email-petition/email-petition";
+import { DEFAULT_MESSAGE } from "@/data/petition-message";
+import { Suspense } from "react";
+
+export function PetitionWithSuspense() {
+  return (
+    <Suspense>
+      {/* Suspense is Required for useSearchParams.*/}
+      <Petition></Petition>
+    </Suspense>
+  );
+}
+
+export function Petition() {
+  const searchParams = useSearchParams();
+  return (
+    <EmailPetition
+      petitionId={process.env.NEXT_PUBLIC_PETITION_ID!}
+      campaignName={process.env.NEXT_PUBLIC_CAMPAIGN_NAME!}
+      defaultMessage={DEFAULT_MESSAGE}
+      onSubmit={onSubmit}
+      debug={searchParams.get("debug") === "true"}
+      test={searchParams.get("test") === "true"}
+    />
+  );
+}
+
+function onSubmit() {
+  window.dataLayer?.push({
+    event: "form_submitted",
+  });
+}
